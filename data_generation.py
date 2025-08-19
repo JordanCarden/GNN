47ba524f0b7f4cad32e605564dea8286d8b117fd
JordanCarden <jcarde7@lsu.edu>
2025-08-19 13:54:36 -0500

    new data

--- data_generation.py
+++ data_generation.py
@@ -4,13 +4,9 @@ import torch
 from torch_geometric.data import Data, InMemoryDataset
 
 class PolymerDataset(InMemoryDataset):
-    """Dataset for polymer graphs."""
-
     def __init__(self, root, transform=None, pre_transform=None):
         super().__init__(root, transform, pre_transform)
-        self.data, self.slices = torch.load(
-            self.processed_paths[0], weights_only=False
-        )
+        self.data, self.slices = torch.load(self.processed_paths[0], weights_only=False)
 
     @property
     def raw_file_names(self):
@@ -24,22 +20,27 @@ class PolymerDataset(InMemoryDataset):
         pass
 
     def process(self):
+        import ast, pandas as pd, torch
+        from torch_geometric.data import Data
+
         df = pd.read_csv(self.raw_paths[0])
         df = df.dropna(subset=["Area AVG", "RG AVG", "RDF Peak"])
+
         data_list = []
         for _, row in df.iterrows():
             input_list = ast.literal_eval(row["Input List"])
+
             features = []
             backbone_indices = {}
             for idx, (n, label) in enumerate(input_list):
                 features.append([1, 0, 0])
                 backbone_indices[n] = idx
+
             edge_index = [[], []]
             for i in range(len(input_list) - 1):
-                edge_index[0].append(i)
-                edge_index[1].append(i + 1)
-                edge_index[0].append(i + 1)
-                edge_index[1].append(i)
+                edge_index[0].extend([i, i + 1])
+                edge_index[1].extend([i + 1, i])
+
             next_node = len(features)
             for n, label in input_list:
                 if label not in ("E0", "S0"):
@@ -47,26 +48,22 @@ class PolymerDataset(InMemoryDataset):
                     bead_type = label[0]
                     prev = backbone_indices[n]
                     for _ in range(m):
-                        features.append(
-                            [0, 1, 0] if bead_type == "S" else [0, 0, 1]
-                        )
+                        features.append([0, 1, 0] if bead_type == "S" else [0, 0, 1])
                         curr = next_node
-                        edge_index[0].append(prev)
-                        edge_index[1].append(curr)
-                        edge_index[0].append(curr)
-                        edge_index[1].append(prev)
+                        edge_index[0].extend([prev, curr])
+                        edge_index[1].extend([curr, prev])
                         prev = curr
                         next_node += 1
-            x = torch.tensor(features, dtype=torch.float)
+
+            x = torch.tensor(features, dtype=torch.float32)
             edge_index = torch.tensor(edge_index, dtype=torch.long)
-            y = torch.tensor(
-                [row["Area AVG"], row["RG AVG"], row["RDF Peak"]],
-                dtype=torch.float,
-            )
+            y = torch.tensor([row["Area AVG"], row["RG AVG"], row["RDF Peak"]],
+                             dtype=torch.float32)
             data_list.append(Data(x=x, edge_index=edge_index, y=y))
+
         data, slices = self.collate(data_list)
         torch.save((data, slices), self.processed_paths[0])
-
+        
 
 if __name__ == "__main__":
     dataset = PolymerDataset(root=".")
